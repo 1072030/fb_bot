3bb7b66c0fb0ef595d5ebaecf811083ce0a92a44
1072030 <46060471+1072030@users.noreply.github.com>
2022-01-21 14:16:48 +0800

    update api

--- fb-bot-api/service/messager-bot-reply.js
+++ fb-bot-api/service/messager-bot-reply.js
@@ -1,22 +1,32 @@
-const fetch = require("node-fetch");
+const axios = require("axios");
 const replyMessager = async (body) => {
-  const data = await fetch(
-    `https://graph.facebook.com/v2.6/me/messages?access_token=${process.env.ACCESS_TOKEN}`,
-    {
-      method: "POST",
-      body: JSON.stringify(body),
-      header: {
-        "Content-Type": "application/json",
-      },
-    }
-  )
-    .then((res) => {
-      return res.json();
-    })
-    .then((result) => {
-      console.log(result);
-    });
-  return data;
+  //   const data = await fetch(
+  //     `https://graph.facebook.com/v2.6/me/messages?access_token=${process.env.ACCESS_TOKEN}`,
+  //     {
+  //       method: "POST",
+  //       body: JSON.stringify(body),
+  //       header: {
+  //         "Content-Type": "application/json",
+  //       },
+  //     }
+  //   )
+  //     .then((res) => {
+  //       return res.json();
+  //     })
+  //     .then((result) => {
+  //       console.log(result);
+  //     });
+  //   return data;
+  const data = await axios({
+    method: "POST",
+    url: `https://graph.facebook.com/v2.6/me/messages?access_token=${process.env.ACCESS_TOKEN}`,
+    data: body,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => {
+    console.log(res);
+  });
 };
 
 module.exports = { replyMessager };
