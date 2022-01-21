f94e4b9dd459e75682a569c0413ce15fc82e9c01
1072030 <46060471+1072030@users.noreply.github.com>
2022-01-21 10:52:44 +0800

    update api

--- app.js
+++ app.js
@@ -1,22 +1,13 @@
 const express = require("express");
 const app = express();
+const verify_webhook = require("./handleEvent/verify_webhook");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use("/", verify_webhook);
 app.get("/", (req, res) => {
   res.status(200).send("health");
 });
-app.post("/webhook", (req, res) => {
-  let body = req.body;
-  if (body.object === "page") {
-    body.entry.forEach(function (entry) {
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
-    });
-    res.status(200).send("EVENT_RECEIVED");
-  } else {
-    res.sendStatus(404);
-  }
-});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
