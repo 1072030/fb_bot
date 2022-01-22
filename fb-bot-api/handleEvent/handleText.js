1cfeb5af1fdc176499949025aef221670e83538b
1072030 <46060471+1072030@users.noreply.github.com>
2022-01-22 15:26:49 +0800

    enable reply text

--- fb-bot-api/handleEvent/handleText.js
+++ fb-bot-api/handleEvent/handleText.js
@@ -1,13 +1,13 @@
 const handleText = async (text) => {
   console.log(text); //這裡可以回傳對應內容
-  // const data = await replyMessager({
-  //   message_type: "text",
-  //   recipient: {
-  //     id: webhook_event.sender.id,
-  //   },
-  //   message: {
-  //     text: "hello world",
-  //   },
-  // });
+  const data = await replyMessager({
+    message_type: "text",
+    recipient: {
+      id: webhook_event.sender.id,
+    },
+    message: {
+      text: "hello world",
+    },
+  });
 };
 module.exports = { handleText };
