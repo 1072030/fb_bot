35526274fac45654db4b53c6b1fb3170108a89b4
1072030 <46060471+1072030@users.noreply.github.com>
2022-02-09 10:06:44 +0800

    user send

--- fb-bot-api/router/verify-webhook.js
+++ fb-bot-api/router/verify-webhook.js
@@ -28,25 +28,25 @@ router.get("/webhook", (req, res) => {
 router.post("/webhook", async (req, res) => {
   let body = req.body;
   console.log("body", body);
-  // console.log("message", body.entry[0].messaging[0]);
-  // if (
-  //   body.object === "page" &&
-  //   body.entry[0].messaging[0].message !== undefined
-  // ) {
-  //   //message 不為空 = 使用者有傳訊息
-  //   // for (let i = 0; i < body.entry.length; i++) {
-  //   //   let webhook_event = body.entry[i].messaging[0];
-  //   //   if (webhook_event.message.text !== undefined) {
-  //   //     handleText(webhook_event, webhook_event.message.text); //處理文字訊息
-  //   //   } else {
-  //   //     console.log("attachments type");
-  //   //   }
-  //   //   console.log("webhook event", webhook_event);
-  //   // }
-  //   res.status(200).send("EVENT_RECEIVED");
-  // } else {
-  //   res.sendStatus(404);
-  // }
+  console.log("message", body.entry[0].messaging[0]);
+  if (
+    body.object === "page" &&
+    body.entry[0].messaging[0].message !== undefined
+  ) {
+    //message 不為空 = 使用者有傳訊息
+    for (let i = 0; i < body.entry.length; i++) {
+      let webhook_event = body.entry[i].messaging[0];
+      if (webhook_event.message.text !== undefined) {
+        handleText(webhook_event, webhook_event.message.text); //處理文字訊息
+      } else {
+        console.log("attachments type");
+      }
+      console.log("webhook event", webhook_event);
+    }
+    res.status(200).send("EVENT_RECEIVED");
+  } else {
+    res.sendStatus(404);
+  }
   res.status(200).send("EVENT_RECEIVED");
 });
 router.post("/refreshPage", async (req, res) => {
