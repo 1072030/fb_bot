321bbd120e577de93b30c383efc58aede7f46ad0
1072030 <46060471+1072030@users.noreply.github.com>
2022-02-18 13:27:20 +0800

    update fb reply

--- fb-bot-api/handleEvent/handleText.js
+++ fb-bot-api/handleEvent/handleText.js
@@ -1,14 +1,33 @@
+const { messageAnalyze } = require("../service/message-analyze");
 const { replyMessager } = require("../service/messager-bot");
 const handleText = async (webhook_event, text) => {
-  console.log(text); //這裡可以回傳對應內容
-  const data = await replyMessager({
-    message_type: "text",
-    recipient: {
-      id: webhook_event.sender.id,
-    },
-    message: {
-      text: "This is webhook reply",
-    },
+  //console.log(text); //這裡可以回傳對應內容
+  const content = messageAnalyze(text);
+  // console.log("content----------------", content);
+  const fruitList = content.content.map((x) => {
+    return x[0] + x[1] + "個";
   });
+  console.log("fruitlist", fruitList);
+  if (content.content === undefined || content.location === undefined) {
+    const data = await replyMessager({
+      message_type: "text",
+      recipient: {
+        id: webhook_event.sender.id,
+      },
+      message: {
+        text: `不確定您的訂單項目，請檢查格式是否錯誤。\n範例 : 芭樂+1、蓮霧+1、香蕉+1，市場取`,
+      },
+    });
+  } else {
+    const data = await replyMessager({
+      message_type: "text",
+      recipient: {
+        id: webhook_event.sender.id,
+      },
+      message: {
+        text: `品項 : ${fruitList}\n地區 : ${content.location}`,
+      },
+    });
+  }
 };
 module.exports = { handleText };
--- fb-bot-api/router/verify-webhook.js
+++ fb-bot-api/router/verify-webhook.js
@@ -37,7 +37,7 @@ router.post("/webhook", async (req, res) => {
     for (let i = 0; i < body.entry.length; i++) {
       let webhook_event = body.entry[i].messaging[0];
       if (webhook_event.message.text !== undefined) {
-        handleText(webhook_event, webhook_event.message.text); //處理文字訊息
+        await handleText(webhook_event, webhook_event.message.text); //處理文字訊息
       } else {
         console.log("attachments type");
       }
@@ -60,11 +60,16 @@ router.post("/refreshPage", async (req, res) => {
     allComments.map(async (x) => {
       if (doc.data().comment_id.indexOf(x.id) == -1) {
         //不存在於firebase需要回復
-        //const publicReply = await PublicReply(x.id, "這是公開的測試回復"); //測試成功
+
         //console.log(x.message);
         console.log(x.id);
         console.log("原始對話: ", x.message);
         const content = messageAnalyze(x.message);
+        let publicReplyMessage = "";
+        content.content.map((x) => {
+          publicReplyMessage.concat(x[0], x[1]);
+        });
+        const publicReply = await PublicReply(x.id); //測試成功
         console.log(content);
         //const secretReply = await SecretReply(x.id, "這是私密的測試回復"); //只能回復管理員 需要權限...
 
