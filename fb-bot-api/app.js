ee62e90c851d0b64283155ad2a8a6f4a2a6021f3
1072030 <46060471+1072030@users.noreply.github.com>
2022-03-07 15:02:50 +0800

    update

--- fb-bot-api/app.js
+++ fb-bot-api/app.js
@@ -3,17 +3,6 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const verify_webhook = require("./router/verify-webhook");
-const {
-  replyMessager,
-  PublicSearch,
-  PublicRead,
-  PublicReply,
-  SecretReply,
-} = require("./service/messager-bot");
-const { firestore } = require("./config/firestore");
-const { messageAnalyze } = require("./service/message-analyze");
-const { orderPrice } = require("./service/order-price");
-const SocketServer = require("ws").Server;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.options(
@@ -27,36 +16,6 @@ app.get("/", (req, res) => {
 });
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port, () => {
+app.listen(port, () => {
   console.log(`listening on ${port}`);
-});
-const wss = new SocketServer({ server });
-wss.on("connection", (ws) => {
-  setInterval(async () => {
-    const allPostContent = await firestore.collection("object-post").get();
-    allPostContent.forEach(async (doc) => {
-      const allComments = await PublicRead(doc.data().post_id);
-      let comments = doc.data().comment_id;
-      //console.log(doc.data().comment_id);
-      allComments.map(async (x) => {
-        if (doc.data().comment_id.indexOf(x.id) == -1) {
-          //不存在於firebase需要回復
-          console.log("---------原始對話: ", x.message);
-          const content = messageAnalyze(x.message);
-          const publicReply = await PublicReply(
-            x.id,
-            "小編已私訊您~請查看留言唷"
-          ); //測試成功
-          const order = orderPrice(content, x.message);
-          const secretReply = await SecretReply(x.id, order); //只能回復管理員 需要權限...
-          comments.push(x.id);
-        } else {
-          // console.log("沒有新留言");
-        }
-        await firestore.collection("object-post").doc(doc.id).update({
-          comment_id: comments,
-        });
-      });
-    });
-  }, 5000);
 });
