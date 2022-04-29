032db647da7926258fe382329f37d570a7b977ca
1072030 <46060471+1072030@users.noreply.github.com>
2022-04-29 14:26:40 +0800

    'update'

--- router/verify-webhook.js
+++ router/verify-webhook.js
@@ -70,6 +70,8 @@ router.post("/webhook", async (req, res) => {
         try {
           const { post_id, comment_id } = req.body.entry[0].changes[0].value;
           console.log(comment_id);
+          const replyMessage = "小編已私訊您，請查看私人訊息呦 :)";
+          PublicReply(comment_id);
         } catch (e) {
           console.log(e);
         }
